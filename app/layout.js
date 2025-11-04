fb80db467b2a1c57edd40bc7e38aabcda4873125
Rushikesh chandanshiv <chandanshiv406@gmail.com>
2025-11-04 16:57:56 +0530

    fix: remove duplicate suppressHydrationWarning from body tag

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
